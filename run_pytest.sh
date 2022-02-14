0f302b7aea6832dd84a3343daad34486554fdb9d
ultimaker-ci <c.schalkwijk@ultimaker.com>
2022-02-14 10:41:10 +0100

    Fixed griffin specific pytest

--- run_pytest.sh
+++ run_pytest.sh
@@ -9,9 +9,7 @@ rm -rf ./.mypy_cache
 rm -rf ./cov_report
 rm -rf ./pstats
 
-USE_DUMMY_DBUS=true \
-    PYTHONPATH=:../dbus-interface-lib:../libpalantir:../libPalantir:../charon:../libCharon:../libsmeagol:../libSmeagol:../marvin-service/src \
-    pytest -c ./ci/cfg/pytest.ini --cov=. --cov-report=html --cov-report=xml --cov-config=./ci/cfg/coverage.ini "${@}" --profile-svg --pstats-dir=./pstats
+pytest -c ./ci/cfg/pytest.ini --cov=. --cov-report=html --cov-report=xml --cov-config=./ci/cfg/coverage.ini "${@}" --profile-svg --pstats-dir=./pstats
 
 py3clean .
 
