8846272bdd4bdb11a24a91950b62590989379a1a
ultimaker-ci <c.schalkwijk@ultimaker.com>
2022-05-23 13:47:37 +0200

    MISP-2111 Fix pylint validation
    Pylint was applied to the entire code base, the change now has pylint
    validation performed only against the changed files instead.

--- run_pylint.sh
+++ run_pylint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+. ci/references.sh
 
 set -eu
 
-pylint --rcfile=./ci/cfg/.pylintrc ./griffin/
+# Log version to track if the non specified version has changed and is breaking stuff.
+pylint --version
+
+if [ -n "${CHANGED_FILES}" ]; then
+    echo "PyLint testing:"
+    echo "${CHANGED_FILES}"
+    # Ignore shellcheck for variable without quotes
+    # shellcheck disable=SC2086
+    pylint --rcfile=./ci/cfg/.pylintrc  ${CHANGED_FILES}
+fi
 
 exit 0
